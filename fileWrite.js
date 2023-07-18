ceff682a40d9a6f3f0104a4c6db63beefe8610a9
Wanguki0807 <139529770+Wanguki0807@users.noreply.github.com>
2023-07-18 12:03:02 +0200

    Add files via upload
    This file demonstrates how approach to the files and how write and update data in file.

---
+++ fileWrite.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+
+fs.writeFile('test.txt', 'Hello World!', function (err) { 
+    if (err)
+        console.log(err);
+    else
+        console.log('Write operation complete.');
+});
+
+fs.writeFile('test.txt', 'I was appended to Hello World!', function (err) { 
+    if (err)
+        console.log(err);
+    else
+        console.log('Write operation complete.');
+});
