b1e2c264532745c527780044134a24345b795df3
Code Developer <139529770+Wanguki0807@users.noreply.github.com>
2023-08-03 18:21:26 +0200

    Spin Box

---
+++ spinBox/spinbox.js
@@ -0,0 +1,6 @@
+function increase() 
+{ document.getElementById("inputBox").value = Number(document.getElementById("inputBox").value)+1;
+ }
+function reduce() {
+ document.getElementById("inputBox").value =  document.getElementById("inputBox").value - 1; 
+ }
