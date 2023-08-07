2394c125e77db6d9ba034e0f93b85bcf28a424b6
Code Developer <139529770+Wanguki0807@users.noreply.github.com>
2023-08-07 04:40:35 -0700

    Update autoConvert1-2-real.py

--- autoConvert1-2-real.py
+++ autoConvert1-2-real.py
@@ -3,9 +3,13 @@ import csv
 import mysql.connector as msql
 from mysql.connector import Error
 import sys
+from datetime import date
 import wx
 import os
 import pyodbc 
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
 from PySide6.QtWidgets import(
     QApplication,
     QMainWindow,
@@ -18,10 +22,18 @@ from PySide6.QtWidgets import(
     QVBoxLayout
 )
 class MainWindow(QMainWindow):
-
+    uploadFileNames = []
     mainCSVPath = ''
     mainName = ''
+    colNum = 0
     txt = ''
+    mainCSVName = ''
+    CreatedTime=''
+    allRowCount=0
+    insertedCount =0
+    updatedCount = 0
+    failedCount = 0
+
     def __init__(self):
         super().__init__()
 ##############################################################
@@ -51,23 +63,38 @@ class MainWindow(QMainWindow):
 
 #################################################################
     def selectFiles(self):
-        def get_path(wildcard):
-            app = wx.App(None)
-            style = wx.FD_OPEN | wx.FD_FILE_MUST_EXIST
-            dialog = wx.FileDialog(None, 'Open', wildcard=wildcard, style=style)
-            if dialog.ShowModal() == wx.ID_OK:
-                path = dialog.GetPath()
-            else:
-                path = None
-            dialog.Destroy()
-            return path
-        self.mainCSVPath =get_path("CSV files (*.csv)|*.csv")
-        head, tail = os.path.split(self.mainCSVPath)
-        self.mainName = tail.replace('.csv','')
-    
-        self.desctexts()
-        # self.uploadBtn.setText(self.mainCSVPath)
-        self.insertData()
+        # def get_path(wildcard):
+        #     app = wx.App(None)
+        #     style = wx.FD_OPEN | wx.FD_MULTIPLE | wx.FD_FILE_MUST_EXIST
+        #     dialog = wx.FileDialog(None, 'Open', wildcard=wildcard, style=style)
+        #     if dialog.ShowModal() == wx.ID_OK:
+        #         path = dialog.GetPath()
+        #     else:
+        #         path = None
+        #     dialog.Destroy()
+        #     return path
+        # self.mainCSVPath =get_path("CSV files (*.csv)|*.csv")
+        # head, tail = os.path.split(self.mainCSVPath)
+        filetypes = (
+                ('CSV files', '*.csv'),
+                # ('All files', '*.*')
+                
+            )
+
+        self.uploadFileNames = fd.askopenfilenames(
+                title='Open a file',
+                initialdir='/',
+                filetypes=filetypes)
+        # self.mainCSVPath = self.uploadFileNames[1]
+        for path in self.uploadFileNames:
+            self.mainCSVPath = path
+            head, tail = os.path.split(self.mainCSVPath)
+            self.mainCSVName = tail
+            self.mainName = tail.replace('.csv','')
+        
+            self.desctexts()
+            # self.uploadBtn.setText(self.mainCSVPath)
+            self.insertData()
 
     def desctexts(self):
         self.txt = self.mainCSVPath
@@ -82,7 +109,7 @@ class MainWindow(QMainWindow):
             #                     password='')
            
             conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=DESKTOP-FB1QUQB;'
+                      'Server=AMSBBSQL2016;'
                       'Database=SDRS;'
                       'UID=SDRSDB;'
                       'PWD=!@QWas12;'
@@ -95,7 +122,8 @@ class MainWindow(QMainWindow):
         file = open(self.mainCSVPath)
         csvreader = csv.reader(file)
         header=[]
-        header = next(csvreader)
+        header = next(csvreader) 
+        colNum = len(header)
         rows=[]
         for row in csvreader:
             rows.append(row)
@@ -105,6 +133,8 @@ class MainWindow(QMainWindow):
         CreateDBQuery ="CREATE TABLE "+ self.mainName + " ("
         for columns in header:
             i=i+1
+            if columns =='':
+                columns = "UnNamed_" + str(i)
             strs = columns + '  varchar(100)'
             if i == num:
                 CreateDBQuery = CreateDBQuery + ' ' + strs + ')'
@@ -123,6 +153,8 @@ class MainWindow(QMainWindow):
         j=0
         for heads in header:
             j = j+1
+            if heads =='':
+                heads = "UnNamed_" + str(j)
             if j == num:
                 query= query + heads +')'
             else:
@@ -130,22 +162,46 @@ class MainWindow(QMainWindow):
         query= query +' VALUES ' 
         number = 1   
         for row in rows:
-            
+            self.allRowCount = self.allRowCount + 1
+            row = row[0:colNum]
             s=str(row)
             s = s.replace('[','(')
             s = s.replace(']',')')
             squery = query + s
-            cursor.execute(squery)
-            
+            try:
+                cursor.execute(squery)
+                self.updatedCount =self.updatedCount + 1
+                self.insertedCount =self.insertedCount + 1
+            except Error as e:
+                self.failedCount = self.failedCount + 1
+                print ("Warning::",e)
+                pass
             sss = str(number) +" data inserted in Databae!"
             number = number+1
             print (sss)
             self.desctext.setText(self.txt)
             conn.commit()
 
-        conn.close()
+        
         self.desctext.setText('Converting completed! ') 
+        today = date.today()
+        self.CreatedTime = today.strftime("%d/%m/%Y")
+        cursor.execute("insert into File_Uploaded(Filename,UploadTimeStamp,LabName,TotalRecords,RecordsInserted,RecordsUpdated,Recordsfailed,Remarks) values (?,?,?,?,?,?,?,?)",\
+            self.mainCSVName,self.CreatedTime,'labName',self.allRowCount,self.insertedCount,self.updatedCount,self.failedCount,'')
+        print("Log file exactly updated")
+        conn.commit()
+
+        conn.close()
         print('Converting completed')
+        self.mainCSVPath = ''
+        self.CreatedTime=''
+        self.allRowCount =0
+        self.insertedCount = 0
+        self.updatedCount = 0
+        self.failedCount = 0
+        self.mainName = ''
+        self.colNum = 0
+        self.txt = ''
 app = QApplication(sys.argv)
 window = MainWindow()
 window.setGeometry(100,100,600,300)
