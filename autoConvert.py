b32d0fa511f6de37ac96b17a9bce8b47a85d5516
Code Developer <139529770+Wanguki0807@users.noreply.github.com>
2023-08-11 00:57:37 -0500

    Update autoConvert.py

--- autoConvert.py
+++ autoConvert.py
@@ -1,14 +1,20 @@
-import pyodbc
+
 import csv
 import mysql.connector as msql
 from mysql.connector import Error
 import sys
+from datetime import date
 import wx
 import os
 import pyodbc 
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
 from PySide6.QtWidgets import(
     QApplication,
     QMainWindow,
+    QCheckBox,
+    QComboBox,
     QPushButton,
     QWidget,
     QTextEdit,
@@ -18,10 +24,20 @@ from PySide6.QtWidgets import(
     QVBoxLayout
 )
 class MainWindow(QMainWindow):
-
+    uploadFileNames = []
     mainCSVPath = ''
     mainName = ''
+    colNum = 0
     txt = ''
+    mainCSVName = ''
+    CreatedTime=''
+    allRowCount=0
+    insertedCount =0
+    updatedCount = 0
+    failedCount = 0
+    mainLabName = ''
+    emailEnabled = 1
+
     def __init__(self):
         super().__init__()
 ##############################################################
@@ -32,6 +48,26 @@ class MainWindow(QMainWindow):
         self.layout.addWidget(self.filetxt)
         self.layout.addWidget(self.uploadBtn)
         self.uploadBtn.clicked.connect(self.selectFiles)
+##############################################################
+        self.labNametxt = QLabel("Select LabName:")
+        self.LabList = QComboBox(self)
+        self.LabList.addItem("FSNSTech")
+        self.LabList.addItem("IEHInc")
+        self.LabList.addItem("Silliker")
+        self.sendemail = QCheckBox("Enable send Email",self)
+        self.layout2 = QHBoxLayout()
+        self.layout2.addWidget(self.labNametxt)
+        self.layout2.addWidget(self.LabList)
+        self.layout2.addSpacing(50)
+        self.layout2.addWidget(self.sendemail)
+        
+        ###################################################
+        
+        self.button = QPushButton("Start Convert")
+        self.button.clicked.connect(self.display_selection)
+##############################################################
+        
+   
 ################################################################
         self.desctxt = QLabel("Working Registry")
         self.desctext = QTextEdit(self)
@@ -43,38 +79,65 @@ class MainWindow(QMainWindow):
         self.vlay = QVBoxLayout(self)
         self.vlay.addLayout(self.layout)
         self.vlay.addSpacing(20)
+        self.vlay.addLayout(self.layout2)
+        # self.vlay.addSpacing(20)
+        # self.vlay.addWidget(self.sendemail)
+        self.vlay.addSpacing(20)
         self.vlay.addLayout(self.layout1)
         self.vlay.addSpacing(20)
+        self.vlay.addWidget(self.button)
         central_widget = QWidget()
         central_widget.setLayout(self.vlay)
         self.setCentralWidget(central_widget)
 
 #################################################################
-    def selectFiles(self):
-        def get_path(wildcard):
-            app = wx.App(None)
-            style = wx.FD_OPEN | wx.FD_FILE_MUST_EXIST
-            dialog = wx.FileDialog(None, 'Open', wildcard=wildcard, style=style)
-            if dialog.ShowModal() == wx.ID_OK:
-                path = dialog.GetPath()
-            else:
-                path = None
-            dialog.Destroy()
-            return path
-        self.mainCSVPath =get_path("CSV files (*.csv)|*.csv")
-        head, tail = os.path.split(self.mainCSVPath)
-        self.mainName = tail.replace('.csv','')
-    
-        self.desctexts()
-        # self.uploadBtn.setText(self.mainCSVPath)
+    def display_selection(self):
+        selection = self.LabList.currentText()
+        self.emailEnabled = self.sendemail.checkState()
+        self.mainLabName = selection
         self.insertData()
+       
+    def selectFiles(self):
+       
+        filetypes = (
+                ('CSV files', '*.csv'),
+                # ('All files', '*.*')
+                
+            )
+
+        self.uploadFileNames = fd.askopenfilenames(
+                title='Open a file',
+                initialdir='/',
+                filetypes=filetypes)
+        # self.mainCSVPath = self.uploadFileNames[1]
+        for path in self.uploadFileNames:
+            self.mainCSVPath = path
+            head, tail = os.path.split(self.mainCSVPath)
+            self.mainCSVName = tail
+            self.mainName = tail.replace('.csv','')
+            self.changeDBName(self.mainName)
+        
+            self.desctexts()
+            # self.uploadBtn.setText(self.mainCSVPath)
+           
 
     def desctexts(self):
         self.txt = self.mainCSVPath
         self.desctext.setText(self.txt)
         
-
-
+    def changeDBName(self,dbName):
+        if(dbName.find("BonelessBeef")!=-1):
+            self.mainName = "BonelessBeef_MicroTest"
+        if(dbName.find("Fat")!=-1):
+            self.mainName = "FinishedProd_FatTest"
+        if(dbName.find("FinishedProd")!=-1):
+            self.mainName = "FinishedProd_MicroTest"
+        if(dbName.find("BonelessBison")!=-1):
+            self.mainName = "BonelessBison_MicroTest"
+        if(dbName.find("BisonFat")!=-1):
+            self.mainName = "Bison_FatTest"
+        if(dbName.find("GroundBison")!=-1):
+            self.mainName = "GroundBison_MicroTest"
     def insertData(self):
         ################  create database   ############################
         try:
@@ -82,7 +145,7 @@ class MainWindow(QMainWindow):
             #                     password='')
            
             conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=DESKTOP-FB1QUQB;'
+                      'Server=DESKTOP-UAA342V;'
                       'Database=SDRS;'
                     #   'UID=SDRSDB;'
                     #   'PWD=!@QWas12;'
@@ -95,25 +158,37 @@ class MainWindow(QMainWindow):
         file = open(self.mainCSVPath)
         csvreader = csv.reader(file)
         header=[]
-        header = next(csvreader)
+        header = next(csvreader) 
+        colNum = len(header)
         rows=[]
         for row in csvreader:
             rows.append(row)
         #################  create table ################################
         num = len(header)
         i = 0
+        
+        stmt = "SHOW TABLES LIKE "+self.mainName
+        cursor.execute(stmt)
+        result = cursor.fetchone()
+        
+        extra = "FileID INT NULL, LastUpdated DATETIME NULL, UpdatedBy  VARCHAR(50) NULL, Remarks NVARCHAR(MAX) NULL)" 
+        # if result:
+            
+        # else:
         CreateDBQuery ="CREATE TABLE "+ self.mainName + " ("
         for columns in header:
             i=i+1
+            if columns =='':
+                columns = "UnNamed_" + str(i)
             strs = columns + '  varchar(100)'
             if i == num:
-                CreateDBQuery = CreateDBQuery + ' ' + strs + ')'
+                CreateDBQuery = CreateDBQuery + ' ' + strs + extra
             else:
                 CreateDBQuery = CreateDBQuery + ' ' +  strs + ','
-
+        CreateDBQuery = CreateDBQuery.replace('-','_')
         cursor = conn.cursor()
-        sqlrs = 'DROP TABLE IF EXISTS '+ self.mainName +';'
-        cursor.execute(sqlrs)
+        # sqlrs = 'DROP TABLE IF EXISTS '+ self.mainName +';'
+        # cursor.execute(sqlrs)
         print('Creating table....')
 
         cursor.execute(CreateDBQuery)
@@ -123,29 +198,56 @@ class MainWindow(QMainWindow):
         j=0
         for heads in header:
             j = j+1
+            if heads =='':
+                heads = "UnNamed_" + str(j)
             if j == num:
-                query= query + heads +')'
+                query= query + heads + extra
             else:
                 query= query + heads +','
         query= query +' VALUES ' 
         number = 1   
         for row in rows:
-            
+            self.allRowCount = self.allRowCount + 1
+            row = row[0:colNum]
             s=str(row)
             s = s.replace('[','(')
             s = s.replace(']',')')
             squery = query + s
-            cursor.execute(squery)
-            
+            squery = squery.replace('-','_')
+            try:
+                cursor.execute(squery)
+                self.updatedCount =self.updatedCount + 1
+                self.insertedCount =self.insertedCount + 1
+            except Error as e:
+                self.failedCount = self.failedCount + 1
+                print ("Warning::",e)
+                pass
             sss = str(number) +" data inserted in Databae!"
             number = number+1
             print (sss)
             self.desctext.setText(self.txt)
             conn.commit()
 
+            
+            self.desctext.setText('Converting completed! ') 
+            today = date.today()
+            self.CreatedTime = today.strftime("%d/%m/%Y")
+            cursor.execute("insert into File_Uploaded(Filename,UploadTimeStamp,LabName,TotalRecords,RecordsInserted,RecordsUpdated,Recordsfailed,Remarks) values (?,?,?,?,?,?,?,?)",\
+                self.mainCSVName,self.CreatedTime,self.mainLabName,self.allRowCount,self.insertedCount,self.updatedCount,self.failedCount,'')
+            print("Log file exactly updated")
+        conn.commit()
+
         conn.close()
-        self.desctext.setText('Converting completed! ') 
         print('Converting completed')
+        self.mainCSVPath = ''
+        self.CreatedTime=''
+        self.allRowCount =0
+        self.insertedCount = 0
+        self.updatedCount = 0
+        self.failedCount = 0
+        self.mainName = ''
+        self.colNum = 0
+        self.txt = ''
 app = QApplication(sys.argv)
 window = MainWindow()
 window.setGeometry(100,100,600,300)
