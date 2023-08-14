351c18b8b7f747559b64365cc6e0dfc62f43dea4
Code Developer <139529770+Wanguki0807@users.noreply.github.com>
2023-08-14 10:38:24 -0500

    Update 1111.py

--- 1111.py
+++ 1111.py
@@ -1,8 +1,1 @@
-from bs4 import BeautifulSoup
-import re
-# import urllib2
 
-url = "./source/index.html"
-page = open(url,encoding='utf-8')
-soup = BeautifulSoup(page.read(),'html.parser')
-texts = str(soup)
