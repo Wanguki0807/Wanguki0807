88bb532db59d83a37897216129dc52bb7c050ee3
Code Developer <139529770+Wanguki0807@users.noreply.github.com>
2023-08-14 06:50:54 -0500

    Update 2023_08_14_093946_create_blog_posts_table.php

--- 2023_08_14_093946_create_blog_posts_table.php
+++ 2023_08_14_093946_create_blog_posts_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateBlogPostsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('blog_posts', function (Blueprint $table) {
@@ -21,12 +16,6 @@ class CreateBlogPostsTable extends Migration
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('blog_posts');
