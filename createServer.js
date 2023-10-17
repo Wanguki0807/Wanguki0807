1e5e0a27b7f21821f1661ab20c51a3831e849c4e
Code Developer <139529770+Wanguki0807@users.noreply.github.com>
2023-10-18 03:34:10 +0800

    Update createServer.js

--- createServer.js
+++ createServer.js
@@ -15,7 +15,6 @@ var server = http.createServer(function (req, res) {   //create web server
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write('<html><body><p>This is student Page.</p></body></html>');
         res.end();
-
         res.end();
         
     }
