2df6caa7817cc522fcfbbf190af13a121b6434c7
Code Developer <139529770+Wanguki0807@users.noreply.github.com>
2023-11-10 14:47:26 +0800

    Update autoConvert1.1.py

--- autoConvert1.1.py
+++ autoConvert1.1.py
@@ -2,4 +2,5 @@
 oh no, there is not anything here.now
 but I can change it now.
 to modify that code I don`t want it.
-because of that reason ,.... I don
+because of that reason ,.... I don`t like hi.
+
